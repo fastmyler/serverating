001ef746ae66a198de3bfdf7948a2bf08cf4dedd
fastmyler <fastmyler@gmail.com>
2024-12-02 12:27:58 +0200

    Addin the functionalities to the wp-review.php

--- wp-content/plugins/wpreview/wpreview.php
+++ wp-content/plugins/wpreview/wpreview.php
@@ -5,7 +5,10 @@
  */
 
  require('includes/cpt-review.php');
-
-
-
+ require('includes/starreview.php');
+ require('includes/affiliate-url.php');
+ require('includes/lowest_price.php');
+ require('includes/expert_score.php');
+ require('includes/top_5.php');
+ require('includes/top_3_related.php');
 ?>
