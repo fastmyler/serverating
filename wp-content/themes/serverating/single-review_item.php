63c141b6e0b27689a892db43b792ed39ee29aedf
fastmyler <fastmyler@gmail.com>
2024-12-07 17:24:29 +0200

    Added ACF field and new Pricing Section in the web hosting reviews // If the Pricing Tab is existing will be dsiaplyed on the page // if not -> Nothing will be displayed

--- wp-content/themes/serverating/single-review_item.php
+++ wp-content/themes/serverating/single-review_item.php
@@ -61,8 +61,17 @@
                 <div class="row" id="side-row">
                     <div class="review_content_div">
                         <div>
-                        <h2 class="review_h6"><?php echo get_the_title(); ?> - Expert Review</h2></div>
+                        <h2 id="review_content" class="review_h6"><?php echo get_the_title(); ?> - Expert Review</h2></div>
                         <?php the_content(); ?>
+                        <?php
+                            $pricing_table = get_field('pricing_table'); // For ACF
+                            if ( $pricing_table ) {
+                            echo '<h2 id="p ricing" class="review_h6">' . get_the_title() . ' - Pricing</h2>';
+                            echo $pricing_table; // Output the value
+                            } else {
+                                echo ''; // 
+                            }
+                        ?>
                         <br>
      <!-- End ofContent -->
      <!-- Comments -->
@@ -87,7 +96,16 @@
                             
                             <li class="sidebar_div_il"><a href="#review_content" class="sidebar_scroll_link">Expert Review</a></li>
                             <li class="sidebar_div_il"><a href="#comments" class="sidebar_scroll_link">Customers Review</a></li>
+                            <?php
+                            $pricing_table = get_field('pricing_table'); // For ACF
+                            if ( $pricing_table ) {
+                            echo '<li class="sidebar_div_il"><a href="#pricing" class="sidebar_scroll_link">Pricing</a></li>'; 
+                            } else {
+                                echo '';  
+                            }
+                             ?>
                             <li class="sidebar_div_il"><a href="#alternative_solutions" class="sidebar_scroll_link">Alternative Solutions</a></li>
+
                             </ul>
                             <div class="side_rat">
                             <h6 style="color:#0073a1;">Expert's Score</h6>
